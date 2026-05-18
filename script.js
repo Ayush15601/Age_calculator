d04a02a13a44e029b3adce69be5dab11143db6b7
Ayush15601 <aayushtewarit@gmail.com>
2026-05-18 18:36:15 +0530

    updated index and script: fixed bugs

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 import { DateTime } from "luxon";
 
-let age_val = document.querySelector(".getage").value;
+let button = document.querySelector(".btn");
+button.addEventListener("click", () => {
+  let age_val = document.querySelector(".getage").value;
 
-if (!age_val) {
-  alert("Ener Age");
-}
+  if (!age_val) {
+    alert("Ener Age");
+  }
 
-let read = luxon.DateTime.fromISO(age_val);
-let currect_date = luxon.DateTime.now();
+  let read = DateTime.fromISO(age_val);
+  let currect_date = DateTime.now();
 
-let diff = currect_date.diff(read, ["year", "month", "day"]).toObject;
+  let diff = currect_date.diff(read, ["years", "months", "days"]).toObject();
 
-let year = Math.floor(year);
-let month = Math.floor(month);
-let day = Math.floor(day);
+  let year = Math.floor(diff.years);
+  let month = Math.floor(diff.months);
+  let day = Math.floor(diff.days);
 
-document.querySelector(".display").innerHTML =
-  `Your ag is ${year}years ${month}month ${day}day`;
+  document.querySelector(".display").innerHTML =
+    `Your age is ${year} years ${month} month ${day} day old`;
+});
